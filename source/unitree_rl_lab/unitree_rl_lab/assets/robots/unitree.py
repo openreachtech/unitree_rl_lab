25a34c5c8d945ee22f4b5c2601bac0a893ef4ee0
dtetsuka <dtetsuka@fundamentum.co.jp>
2026-06-25 21:43:25 +0900

    unitree.pyのコミット棄却

--- source/unitree_rl_lab/unitree_rl_lab/assets/robots/unitree.py
+++ source/unitree_rl_lab/unitree_rl_lab/assets/robots/unitree.py
@@ -18,7 +18,7 @@ from isaaclab.utils import configclass
 from unitree_rl_lab.assets.robots import unitree_actuators
 
 UNITREE_MODEL_DIR = "path/to/unitree_model"  # Replace with the actual path to your unitree_model directory
-UNITREE_ROS_DIR = "/home/tetsuka/isaacsim/unitree_ros"  # Replace with the actual path to your unitree_ros package
+UNITREE_ROS_DIR = "path/to/unitree_ros"  # Replace with the actual path to your unitree_ros package
 
 
 @configclass
@@ -93,12 +93,12 @@ class UnitreeUrdfFileCfg(sim_utils.UrdfFileCfg):
 """ Configuration for the Unitree robots."""
 
 UNITREE_GO2_CFG = UnitreeArticulationCfg(
-    spawn=UnitreeUrdfFileCfg(
-        asset_path=f"{UNITREE_ROS_DIR}/robots/go2_description/urdf/go2_description.urdf",
+    # spawn=UnitreeUrdfFileCfg(
+    #     asset_path=f"{UNITREE_ROS_DIR}/robots/go2_description/urdf/go2_description.urdf",
+    # ),
+    spawn=UnitreeUsdFileCfg(
+        usd_path=f"{UNITREE_MODEL_DIR}/Go2/usd/go2.usd",
     ),
-    #spawn=UnitreeUsdFileCfg(
-    #    usd_path=f"{UNITREE_MODEL_DIR}/Go2/usd/go2.usd",
-    #),
     init_state=ArticulationCfg.InitialStateCfg(
         pos=(0.0, 0.0, 0.4),
         joint_pos={
